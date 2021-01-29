7e9a6a79df3513c77ba24463b0565ccccefdeebb
Davide Monesi <davide.monesi@hotmail.it>
2021-01-29 19:15:42 +0100

    Web server funzionante dopo modifiche

--- src/main/java/com/ATMproject/SOA/Config/UTPasswordCallback.java
+++ src/main/java/com/ATMproject/SOA/Config/UTPasswordCallback.java
@@ -13,34 +13,33 @@ import org.apache.wss4j.common.ext.WSPasswordCallback;
 public class UTPasswordCallback implements CallbackHandler {
 
 	//semplificazione: in memory database per lo storage di username e password corrispondenti
-		//che possono essere utilizzate dai client
-		private Map<String, String> passwords = new HashMap<>();
+	//che possono essere utilizzate dai client
+	private Map<String, String> passwords = new HashMap<>();
 		
-		//i client devono usare questi username e password altrimenti non
-		//possono usare questa web application
-		public UTPasswordCallback() {
-			passwords.put("pippo", "segreto");
-		}
+	//i client devono usare questi username e password altrimenti non
+	//possono usare questa web application
+	public UTPasswordCallback() {
+		passwords.put("pippo", "segreto");
+		passwords.put("myservicekey", "skpass");
+	}
 		
-		@Override
-		public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-			//callbacks hanno lo username che arriva nella request, bisogna trovare
-			//la password corretta (match) e settare la password nel callback relativa
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		//callbacks hanno lo username che arriva nella request, bisogna trovare
+		//la password corretta (match) e settare la password nel callback relativa
 			
-			for(Callback callback : callbacks) {
-				WSPasswordCallback passwordCallBack = (WSPasswordCallback) callback;
-				//getIdentifier ritorna lo username poi prendiamo la password corrispondete
-				//dall'hashmap
-				String password = passwords.get(passwordCallBack.getIdentifier());
-				//se l'utente ha una password nell'hashmap settiamo la password
-				//nel passwordCallBack
-				if(password != null) {
-					passwordCallBack.setPassword(password);
-					return;
-				}
-			}
-
+		for(Callback callback : callbacks) {
+			WSPasswordCallback passwordCallBack = (WSPasswordCallback) callback;
+			//getIdentifier ritorna lo username poi prendiamo la password corrispondete
+			//dall'hashmap
+			String password = passwords.get(passwordCallBack.getIdentifier());
+			//se l'utente ha una password nell'hashmap settiamo la password
+			//nel passwordCallBack
+			if(password != null) {
+				passwordCallBack.setPassword(password);
+				return;
+			}			
 		}
-
-
+	}
+	
 }
--- src/main/java/com/ATMproject/SOA/Config/WebServiceConfig.java
+++ src/main/java/com/ATMproject/SOA/Config/WebServiceConfig.java
@@ -8,6 +8,7 @@ import javax.xml.ws.Endpoint;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 import org.apache.wss4j.common.ConfigurationConstants;
 import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,44 +19,72 @@ import com.ATMproject.SOA.*;
 
 @Configuration
 public class WebServiceConfig {
-	
+
 	@Autowired
 	private Bus bus;
-	
+
 	@Bean
 	public Endpoint endpoint() {
-		EndpointImpl endpoint = new EndpointImpl(bus, new ATMwsImpl());
-		
-		//l'endpoint del ws viene esposto a questo url
-		endpoint.publish("/ATMservice");
-		
-		//hashmap che conterrà le specifiche per l'interceptor
-		Map<String, Object> inProps = new HashMap<>();
-		
-		//set delle azioni che deve fare l'interceptor
-		inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
-		//affinchè l'azione definita prima funzioni bisogna definire 2 parametri
-		//di che tipo è la password
-		inProps.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
-		//PW_CALLBACK_CLASS fornisce info sulla password per un dato utente
-		//UTPasswordCallback deve essere creata come classe, estende l'interfaccia CallbackHandler
-		inProps.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
-		
-		//input interceptor usato per gestire una richiesta in ingresso
-		WSS4JInInterceptor wssIn = new WSS4JInInterceptor(inProps);
-		//configurazione dell'interceptor nell'endpoint
-		endpoint.getInInterceptors().add(wssIn);
-		
-		
-		//output interceptor usato per gestire i messaggi in uscita
 
-		
-		
-		
+		EndpointImpl endpoint = new EndpointImpl(bus, new ATMwsImpl());
+
+		// l'endpoint del ws viene esposto a questo url
+		endpoint.publish("/ATMservice");
+
+		// ------------ INPUT INTERCEPTOR: autenticazione, decifratura, verifica firma
+		// --------
+
+		Map<String, Object> inProps = new HashMap<>();
+
+		// azioni che deve compiere l'input interceptor
+		inProps.put(ConfigurationConstants.ACTION, "UsernameToken Encrypt Signature Timestamp");
+
+		// autenticazione
+		inProps.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+		// PW_CALLBACK_CLASS fornisce info sulla password per un dato utente
+		inProps.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+
+		// decifratura
+		inProps.put(ConfigurationConstants.DEC_PROP_FILE, "etc/serviceKeystore.properties");
+
+		// verfica firma
+		inProps.put(ConfigurationConstants.SIG_PROP_FILE, "etc/serviceKeystore.properties");
+
+		// input interceptor usato per gestire una richiesta in ingresso
+		WSS4JInInterceptor wssIn = new WSS4JInInterceptor(inProps);
+		// configurazione dell'interceptor nell'endpoint
+		endpoint.getInInterceptors().add(wssIn);
+
+		// ----------- OUT INTERCEPTOR: cifratura, firma, timestamp ----------------
+		Map<String, Object> outProps = new HashMap<>();
+
+		// azioni che deve compiere l'output interceptor
+		outProps.put(ConfigurationConstants.ACTION, "Timestamp Signature Encrypt");
+		// PW_CALLBACK_CLASS fornisce info sulla password per un dato utente
+		outProps.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+
+		// cifratura
+		outProps.put(ConfigurationConstants.ENC_PROP_FILE, "etc/serviceKeystore.properties");
+		outProps.put(ConfigurationConstants.ENCRYPTION_USER, "myclientkey");
+		// per verdere l'elemento SIgnature commentare la riga sotto
+		outProps.put(ConfigurationConstants.ENCRYPTION_PARTS,
+				"{Element}{http://www.w3.org/2000/09/xmldsig#}Signature;{Content}{http://schemas.xmlsoap.org/soap/envelope/}Body");
+
+		// firma
+		outProps.put(ConfigurationConstants.SIG_PROP_FILE, "etc/serviceKeystore.properties");
+		outProps.put(ConfigurationConstants.SIGNATURE_USER, "myservicekey");
+		outProps.put(ConfigurationConstants.SIGNATURE_PARTS,
+				"{Element}{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp;{Element}{http://schemas.xmlsoap.org/soap/envelope/}Body");
+
+		// tempo di vita del timestamp (di default è 5 minuti)
+		outProps.put("timeToLive", "30");
+
+		// output interceptor usato per gestire i messaggi in uscita
+		WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
+		// configurazione dell'interceptor nell'endpoint
+		endpoint.getOutInterceptors().add(wssOut);
+
 		return endpoint;
 	}
 
 }
-
-
-
